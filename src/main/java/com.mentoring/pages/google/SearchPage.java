d45d8d104cf3349a20448eeee626fc92be93c3e8
stasyarudenko <stacy.rudenko@gmail.com>
2019-09-26 03:06:19 +0300

    updated readme file

--- src/main/java/com.mentoring/pages/google/SearchPage.java
+++ src/main/java/com.mentoring/pages/google/SearchPage.java
@@ -2,6 +2,7 @@ package com.mentoring.pages.google;
 
 import com.mentoring.pages.BasePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 
